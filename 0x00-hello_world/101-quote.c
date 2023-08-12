f787997273d1141c8c3a51bf3a0d186054ff38dc
Abdualghani <ahmedabdalghani41@gmail.com>
2023-08-12 09:09:18 -0700

    0x03. Shell, init files, variables and expansions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
  * main - Entry Point
  * Description: Print without prinf and put
  * Return: 1 not successful
- */
+*/
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59)	
+	write(1.quo.59);
 	return (1);
 }

cf125faa7977644516c58fb4ab9ea6288eeb4b69
Abdualghani <ahmedabdalghani41@gmail.com>
2023-08-12 07:19:45 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that's prints with pt function
+ *
+ * Return: Always 0 (sucsses)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

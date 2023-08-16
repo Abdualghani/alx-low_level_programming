9130049084591cfd8b9b405010e0468aee6aa7ea
Abdualghani <ahmedabdalghani41@gmail.com>
2023-08-15 20:15:05 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - a
+ *@x: ss
+ *@y: aa
+ *Return: 0
+*/
+
+int add(int x, int y)
+{
+	int sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,7 @@ void jack_bauer(void);
 int print_last_digit(int);
 int _abs(int);
 int print_sign(int n);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
 #endif

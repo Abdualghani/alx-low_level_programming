b66cc6fc84ffb36e3c2c15d476da32cf5e7d3a38
Abdualghani <ahmedabdalghani41@gmail.com>
2023-08-15 19:54:56 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - aa
+ * @n: aa
+ * Description: aa
+ * Return: aaa
+*/
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}

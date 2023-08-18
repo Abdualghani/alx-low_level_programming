3afba6271d017d9a0adc7f2b3b80ee9da4c0df24
Abdualghani <ahmedabdalghani41@gmail.com>
2023-08-17 18:45:55 -0700

    0x04. C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - fun
+ * Description: aa
+ * Return: a
+*/
+
+
+void more_numbers(void)
+{
+	int it;
+
+	for (it = 1; it <= 10; it++)
+	{
+		int x = 0;
+
+		do {
+			_putchar(x + 48);
+			x++;
+		} while (x >= 0 && x <= 9);
+
+		int y = 0;
+
+		do {
+			_putchar(49);
+			_putchar(y + 48);
+			y++;
+		} while (y >= 0 && y <= 4);
+		_putchar('\n');
+	}
+}

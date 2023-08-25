a163dd15e85a72515c40ac7948b6984b29f5b03e
Abdualghani <ahmedabdalghani41@gmail.com>
2023-08-25 16:14:08 -0700

    0x06. C

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *  infinite_add - add 2 strings.
- *   @n1: string1.
- *    @n2: string2.
+ *  infinite_add - function
+ *   @n1: char
+ *    @n2: char
  *     @r: buffer
  *      @size_r: buffer size
- *       Return: r + 1
+ *       Description: adds two numbers from string
+ *        Return: sum of two integers
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
-		
-	while (n1[a_len] != '\0')
-		a_len++;
-	while (n2[b_len] != '\0')
-		b_len++;
-	if (a_len > b_len)
-		biggest = a_len;
-	else
-		biggest = b_len;
-	if ((biggest + 1) >= size_r)
-		return (0);
-	r[biggest + 1] = '\0';
-		
-	while (biggest >= 0)
+	int count, count2;
+
+	while (n1[count] != '\0')
+		count++;
+	while (n2[count2] != '\0')
+		count2++;
+
+	*r = *(r + size_r);
+	while (n1[count] > 0 || n1[count2] > 0)
 	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
-		if (a_len > 0 && b_len > 0)
-			sum = a + b + carry;
-		else if (a_len < 0 && b_len > 0)
-			sum = b + carry;
-		else if (a_len > 0 && b_len < 0)
-			sum = a + carry;
+		if (n1[count] + n2[count2] > 0)
+			*r = n1[count - 1] + n2[count2 - 1] + 1;
 		else
-			sum = carry;
-				
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
-		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[biggest] = sum;
-		a_len--;
-		b_len--;
-		biggest--;
+			*r = n1[count] + n2[count2];
+		count--;
+		count2--;
 	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+	return (r);
 }
